e31284d6b417e6646721d93655ef5f056ff29460
Laís Cavalcanti <cavalcantislais@gmail.com>
2021-02-28 17:33:01 -0300

    alterações linty

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     'react',
   ],
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
-    'import/prefer-default-export': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'import/prefer-default-export': 'off',
   },
 };
